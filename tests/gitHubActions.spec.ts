b41a1303f9fceec120d8081360e8eed27fc1b37c
Bharath07-QA <bharathevuri.qa@gmail.com>
2025-06-04 22:57:00 +0530

    Bug Fixes

--- tests/gitHubActions.spec.ts
+++ tests/gitHubActions.spec.ts
@@ -20,7 +20,7 @@ test("Practice Test 3", async ({ page }) => {
 console.log("Starting Practice Test 3");
 await page.goto("https://www.saucedemo.com/");
 console.log(await page.title());
-expect(page).toHaveTitle("Swag Labs wrong")
+expect(page).toHaveTitle("Swag Labs")
 console.log("Ending Practice Test 3");
 })
 
